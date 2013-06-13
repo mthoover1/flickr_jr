e2d47eb006ac3bfe65759bbc7e645a8990ad216d
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-13 17:28:30 -0500

    initial home page styling

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,3 +2,16 @@ get '/' do
   # Look in app/views/index.erb
   erb :index
 end
+
+get '/users/:user_id/albums' do
+
+end
+
+get '/albums/:album_id/:photo_id' do
+
+end
+
+get '/albums/:album_id' do
+
+end
+
